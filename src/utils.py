1b3463211553975f0eddf1a4dae77d89b7161784
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-04-25 13:08:41 +0530

    completed

--- src/utils.py
+++ src/utils.py
@@ -4,11 +4,13 @@
 import os 
 import sys
 from src.exception import CustomException
+import dill
 
 import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -28,7 +30,7 @@ def save_object(file_path, obj):
 
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models) -> dict:
+def evaluate_models(X_train,y_train,X_test,y_test,models,param=None) -> dict:
 
     try:
         report = {}
@@ -55,5 +57,9 @@ def evaluate_models(X_train,y_train,X_test,y_test,models) -> dict:
         
 
 
-
-    
+def load_object(file_path):
+    try:
+        with open(file_path,mode='rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
